286f37ba711077dcb7c2bd9dc65d0742c70e831f
ahbing <givemearainbowlife@gmail.com>
2016-10-29 13:27:08 +0800

    add test basic

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
 ;(function(name, context, factory) {
+  /* istanbul ignore if */
   if (typeof defined == 'function') defined(factory)
   else if (typeof module != 'undefined') module.exports = factory()
-  else context[name] = factory()
+  /* istanbul ignore next */
+  else context[name] = factory() 
 })('EventMitter', this, function() {
-  var proto = EventEmitter.prototype
+  var proto = EventEmitter.prototype;
   function EventEmitter() {
-    this._events = {}
+    this._events = {};
   }
   proto.on = function(event, listeners) {
     if (typeof listeners === 'function') {
-      listeners = [listeners]
+      listeners = [listeners];
     }
     if (!Array.isArray(listeners)) {
-      throw new Error(listeners + 'is not a function or function[]')
+      throw new Error(listeners + 'is not a function or function[]');
     }
-    var events = this._events[event] || (this._events[event] = [])
+    var events = this._events[event] || (this._events[event] = []);
     for (var i = 0, n = listeners.length; i < n; i++) {
-      listeners[i].listener = listeners[i]
-      events.push(listeners[i])
+      listeners[i].listener = listeners[i];
+      events.push(listeners[i]);
     }
-    return this
+    return this;
   }
   function _oncewrap(target, event, listensers) {
-    var fired = false
+    var fired = false;
     function g() {
-      target.removeListener(event, g)
+      target.removeListener(event, g);
       if (!fired) {
-        fired = true
+        fired = true;
         if (typeof listensers === 'function') {
-          listensers = [listensers]
+          listensers = [listensers];
         }
         if (Array.isArray(listensers)) {
           for (var i = 0, l = listensers.length; i < l; i++) {
-            listensers[i].apply(target, arguments)
+            listensers[i].apply(target, arguments);
           }
         }
       }
     }
-    return g
+    return g;
   }
   proto.once = function(event, listeners) {    
-    this.on(event, _oncewrap(this, event, listeners))
-    return this
+    this.on(event, _oncewrap(this, event, listeners));
+    return this;
   }
   proto.emit = function(event) {
-    var events = this._events[event]
-    if (!events) return
-    var i = events.length
+    var events = this._events[event];
+    if (!events) return;
+    var i = events.length;
     while (i--) {
-      (events[i].listener || events[i])()
+      (events[i].listener || events[i])();
     }
-    return this
+    return this;
   }
   proto.removeListener = function(event, listners) {
-    var events = this._events[event]
-    if (!events) return 
-    var i = events.length
+    var events = this._events[event];
+    if (!events) return;
+    var i = events.length;
     if (typeof listners === 'function') {
       while(i--) {
         if ((events[i].listener || events[i]) === listners) {
-          events.splice(i, 1)
+          events.splice(i, 1);
         }
       }
     } else if(Array.isArray(listners)) {
       for (var j = 0, l = listners.length; j < l; j++) {
-        this.removeListener(event, listners[j])
+        this.removeListener(event, listners[j]);
       }
     }
-    return this
+    return this;
   }
 
   proto.off = function(event) {
-    delete this._events[event]
-    return this
+    delete this._events[event];
+    return this;
   }
-  return EventEmitter
+  return EventEmitter;
 });
 
 
---
+++ test.js
@@ -0,0 +1,6 @@
+var EventEmitter = require('./index');
+var mocha = require('mocha');
+var chai = require('chai');
+var sinon = require('sinon');
+var expect = chai.expect;
+
